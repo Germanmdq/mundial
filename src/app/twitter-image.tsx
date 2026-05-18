37a565f1820a7d86924a95c1f6d71a97faff1eaf
German <german@MacBook-Air-de-German.local>
2026-05-18 08:57:21 -0300

    Fix twitter image route exports for Vercel build

--- src/app/twitter-image.tsx
+++ src/app/twitter-image.tsx
@@ -1,1 +1,11 @@
-export { alt, contentType, default, runtime, size } from "./opengraph-image";
+import OpenGraphImage from "./opengraph-image";
+
+export const alt = "Mundial entre Amigos";
+export const size = {
+  width: 1200,
+  height: 630,
+};
+export const contentType = "image/png";
+export const runtime = "edge";
+
+export default OpenGraphImage;
